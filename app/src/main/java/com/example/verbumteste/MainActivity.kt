40591668cd328eb4253e48976310c9948edd3822
Eujoice <joicebc14@gmail.com>
2026-04-28 12:29:17 -0300

    Tentativa de fazer a bottom_nav_bar funcionar

--- app/src/main/java/com/example/verbumteste/MainActivity.kt
+++ app/src/main/java/com/example/verbumteste/MainActivity.kt
@@ -2,16 +2,37 @@ package com.example.verbumteste
 
 import android.os.Bundle
 import android.view.View
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.verbumteste.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener { // Classe para tornar a BottomNav navegável
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Esconde a status bar
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).hide(WindowInsetsCompat.Type.statusBars())
+
+        binding.bottom_nav_bar.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                // parei aqui, tentando fazer a bottom_nav_bar funcionar
+            }
+        }
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_bar)
         bottomNav.visibility = View.GONE // esconde até o login
