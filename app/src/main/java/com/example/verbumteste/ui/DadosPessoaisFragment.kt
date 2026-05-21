77f3bea2375d339f87bf6d32e7fc5ddcb6e39fc0
Eujoice <joicebc14@gmail.com>
2026-05-21 18:27:25 -0300

    Comeco de conexão para authentication

--- app/src/main/java/com/example/verbumteste/ui/DadosPessoaisFragment.kt
+++ app/src/main/java/com/example/verbumteste/ui/DadosPessoaisFragment.kt
@@ -6,16 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.verbumteste.R
+import com.example.verbumteste.databinding.FragmentDadosPessoaisBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class DadosPessoaisFragment : Fragment() {
+
+    private var _binding: FragmentDadosPessoaisBinding? = null
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dados_pessoais, container, false)
+        _binding = FragmentDadosPessoaisBinding.inflate(inflater, container, false)
+        return binding.root // ta dando erro, mas logo ele vai parar de show
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth
     }
 
 
