af267108e53cbba068ce594f5c5bf0f8190b7673
Eujoice <joicebc14@gmail.com>
2026-06-02 12:48:11 -0300

    Início da conexão com os livros do banco de dados

---
+++ app/src/main/java/com/example/verbumteste/Livro.kt
@@ -0,0 +1,30 @@
+package com.example.verbumteste
+
+data class Livro(
+    val ano_publicacao: String ="",
+    val autor: String ="",
+    val avaliacao: String ="",
+    val avaliacao_media: String ="",
+    val capa: String ="",
+    val colecao: String ="",
+    val data_adicao: String ="",
+    val data_devolucao_prevista: String ="",
+    val data_emprestimo: String ="",
+    val editora: String ="",
+    val emprestado_por: String ="",
+    val exemplares_totais: String ="",
+    val genero: String ="",
+    val id: String ="",
+    val id_emprestimo_atual: String ="",
+    val idioma_original: String ="",
+    val isbn: String ="",
+    val local_publicacao: String ="",
+    val localizacao: String ="",
+    val paginas: String ="",
+    val resumo: String ="",
+    val sinopse: String ="",
+    val status: String ="",
+    val titulo: String ="",
+    val total_avaliacoes: String ="",
+    val traducao: String =""
+)
