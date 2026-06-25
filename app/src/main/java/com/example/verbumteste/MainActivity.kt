6311ac2add0377860895a3129760464c4aaf5528
Eujoice <you@example.com>
2026-06-25 20:31:40 -0300

    Tela cheia no mainactivity.kt e botão ver mais na tela de acervo

--- app/src/main/java/com/example/verbumteste/MainActivity.kt
+++ app/src/main/java/com/example/verbumteste/MainActivity.kt
@@ -37,20 +37,7 @@ class MainActivity : AppCompatActivity() {
             view.setPadding(0, 0, 0, systemBars.bottom)
             insets
         }
-// ta um pequeno erro, a parte de cima tá branca
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
-
-            val statusBarInsets = insets.getInsets(WindowInsetsCompat.Type.statusBars())
-            val cutoutInsets = insets.getInsets(WindowInsetsCompat.Type.displayCutout())
-
-
-            val topPadding = maxOf(statusBarInsets.top, cutoutInsets.top)
-
-
-            view.setPadding(view.paddingLeft, topPadding, view.paddingRight, view.paddingBottom)
-
-            insets
-        }
+// Erro parcialmente consertado
 
         // 5. Navigation Component
         val navHostFragment = supportFragmentManager
